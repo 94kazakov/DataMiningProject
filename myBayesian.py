c9634684363e0428eff14a5ad756f51e9efda10a
Tyler <tyler.fox09@gmail.com>
2015-04-14 11:43:57 -0600

    Added code for getting the reaction probs

--- myBayesian.py
+++ myBayesian.py
@@ -5,6 +5,7 @@ from bayesian.bbn import build_bbn
 import csv
 
 drug_prob = {}
+react_prob = {}
 
 def get_probs(my_file):
   '''
@@ -32,7 +33,9 @@ def get_probs(my_file):
 
   ## Do Work
   drug_count = {}
+  react_count = {}
   global drug_prob
+  global react_prob
   total_reports = len(data)
   for report in data:
     drugs = report[0].split(';')
@@ -47,20 +50,30 @@ def get_probs(my_file):
       else:
         drug_count[drug] = drug_count[drug] + 1
 
-    ## If drug X is present, what is the probability 
-    ## Effect Y is present as well?
-    for reaction in reactions:
-      pass
+      ## If drug X is present, what is the probability 
+      ## Effect Y is present as well?
+      for reaction in reactions:
+        if drug not in react_count:
+          react_count[drug] = {}
+          react_count[drug][reaction] = 1
+        else:
+          if reaction not in react_count[drug]:
+            react_count[drug][reaction] = 1
+          else:
+            react_count[drug][reaction] = react_count[drug][reaction] + 1
 
 
   for drug in drug_count:
     drug_prob[drug] = float(drug_count[drug])/float(total_reports)
 
-  #print drug_prob
-
-
-
-  #return drug_prob,'Undefined'
+  for drug in react_count:
+    for reaction in react_count[drug]:
+      if drug not in react_prob:
+        react_prob[drug] = {}
+      react_prob[drug][reaction] = float(react_count[drug][reaction])/float(drug_count[drug])
+      print react_prob[drug][reaction], drug, reaction
+  #print react_prob['IMIGLUCERASE']
+  #return drug_prob
 
 
 
@@ -70,10 +83,8 @@ def f_drugs(drug):
   '''
   global drug_prob
   if drug in drug_prob:
-    #print drug, drug_prob[drug]
     return drug_prob[drug]
   else:
-    print 0
     return 0
 
 
@@ -81,8 +92,13 @@ def f_reactions(drug, reaction):
   '''
   Need frequency of a reaction appears based on the drugs
   '''
-  #print 'react'
-  return 1
+  global react_prob
+  if drug in react_prob:
+    if reaction in react_prob[drug]:
+      print 'YES'
+      return react_prob[drug][reaction]
+
+  return 0
 
 
 if __name__=='__main__':
@@ -92,6 +108,43 @@ if __name__=='__main__':
     f_drugs,
     f_reactions,
     domains=dict(
-      drug=list(drug_prob)[:5],
-      reaction=list(drug_prob)[6:10]))
-  g.q()
+      drug=list(drug_prob)[:1],
+      reaction=list(react_prob)[:1]))
+  #g.q()
+  g.q(drug='IMIGLUCERASE')
+
+'''
+import pystan
+
+schools_code = """
+data {
+    int<lower=0> J; // number of schools
+    real y[J]; // estimated treatment effects
+    real<lower=0> sigma[J]; // s.e. of effect estimates
+}
+parameters {
+    real mu;
+    real<lower=0> tau;
+    real eta[J];
+}
+transformed parameters {
+    real theta[J];
+    for (j in 1:J)
+    theta[j] <- mu + tau * eta[j];
+}
+model {
+    eta ~ normal(0, 1);
+    y ~ normal(theta, sigma);
+}
+"""
+
+schools_dat = {'J': 8,
+               'y': [28,  8, -3,  7, -1,  1, 18, 12],
+               'sigma': [15, 10, 16, 11,  9, 11, 10, 18]}
+
+fit = pystan.stan(model_code=schools_code, data=schools_dat,
+                  iter=1000, chains=4)
+
+
+print fit
+'''
